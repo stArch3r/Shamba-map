4d591e41cd833a5fcba3e1afad515c041e9aa7fe
Dave-3 <mdnjenga@gmail.com>
2020-11-17 17:00:30 +0300

    landing page images

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,6 +2,8 @@
 
 /* @var $this yii\web\View */
 
+use yii\helpers\Url;
+
 $this->title = 'SHAMBANI';
 ?>
 
@@ -20,21 +22,39 @@ $this->title = 'SHAMBANI';
     <div class="explore">
         <h1>Properties</h1>
         <h1>Near Me</h1>
-       <p><button type="button" class="btn btn-outline-secondary">Explore Nearby Properties</button></p>
+       <p><a href="<?= Url::to(['property/index'])?>"><button type="button" class="btn btn-outline-secondary">Explore Nearby Properties</button></a></p>
     </div>
+    <br><br>
 
 </div>
 
 <div class="row1">
 <div class="row">
-    <div class="col-md-3">
-       
-             <img class="card-img" src="<?= Yii::$app->request->baseUrl;?>/images/hse.jpeg" alt="Card image cap">
-            
-           <h3> Mombasa </h3> 
-            <h6>7 hours drive  </h6> 
-          
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-4">
+            <img src="<?= Yii::$app->request->baseUrl;?>/images/hse1.jpeg" class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This</p>
+            </div>
+            </div>
+        </div>
     </div>
+    <div class="col-md-3">
+        <div class="row">
+                <div class="col-md-6">
+                    
+                <img class="card-img" src="<?= Yii::$app->request->baseUrl;?>/images/hse1.jpeg" alt="Card image cap">
+                </div>
+                <div class="col-md-6">
+            <h3> Mombasa </h3> 
+                <h6>7 hours drive  </h6> 
+                </div>
+            </div>
+        </div>
     <div class="col-md-3">
         <div class="row">
             <div class="col-md-8">
@@ -147,40 +167,42 @@ $this->title = 'SHAMBANI';
         </div>
     </div>
 </div>
+    <div class="jumbotron">
+        <div class="container">
 
-    <div class="row1">
-        <div class="row">
-            <div class="col">            
-            <h3> Meet Online Experiences </h3> 
-            <h6> Interactive activities you can do together, led by expert hosts.</h6> 
-            </div>
-        </div> 
-        <div class="row">
-            <div class="col-md-6">
-                <img class="card-img1" src="<?= Yii::$app->request->baseUrl;?>/images/land.jpeg" alt="Card image cap">
-                Craft cocktails and talk gender with a bartender
-            </div>
-            <div class="col-md-6">
-                <div class="row">
-                    <div class="col-md-6">
-                        <img class="card-img2" src="<?= Yii::$app->request->baseUrl;?>/images/hse3.jpeg" alt="Card image cap">
-                        Craft cocktails and talk gender with a bartender
-                    </div>
-                    <div class="col-md-6">
-                        <img class="card-img2" src="<?= Yii::$app->request->baseUrl;?>/images/hse4.jpeg" alt="Card image cap">
-                        Craft cocktails and talk gender with a bartender
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                    <img class="card-img3" src="<?= Yii::$app->request->baseUrl;?>/images/sale.jpeg" alt="Card image cap">
+            <h3 class="text-white"> Meet Online Experiences </h3> 
+            <h6 class="text-white"> Interactive activities you can do together, led by expert hosts.</h6> 
+       
+        
+            <div class="row">
+                <div class="col-md-6">
+                    <img class="card-img1" src="<?= Yii::$app->request->baseUrl;?>/images/land.jpeg" alt="Card image cap">
                     Craft cocktails and talk gender with a bartender
                 </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img class="card-img2" src="<?= Yii::$app->request->baseUrl;?>/images/hse3.jpeg" alt="Card image cap">
+                            Craft cocktails and talk gender with a bartender
+                        </div>
+                        <div class="col-md-6">
+                            <img class="card-img2" src="<?= Yii::$app->request->baseUrl;?>/images/hse4.jpeg" alt="Card image cap">
+                            Craft cocktails and talk gender with a bartender
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                        <img class="card-img3" src="<?= Yii::$app->request->baseUrl;?>/images/sale.jpeg" alt="Card image cap">
+                        Craft cocktails and talk gender with a bartender
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
-        </div>
     </div>
+ 
+ 
+  
 
     <div class="row1">
     <div class="row">
