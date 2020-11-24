07680df299e92081309583fe66d7ab45f6acbbf5
stArch3r <kenmunenenyaga@gmail.com>
2020-11-24 08:38:14 +0300

    new changes
    modal

--- frontend/views/property/book.php
+++ frontend/views/property/book.php
@@ -1,32 +1,27 @@
 <?php
-use dosamigos\datepicker\DatePicker;
-use yii\helpers\ArrayHelper;
+
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+
 /* @var $this yii\web\View */
-/* @var $model frontend\models\Borrowedbook */
+/* @var $model frontend\models\Listing */
 /* @var $form ActiveForm */
 ?>
-<div class="borrowbook">
-    <?php $form = ActiveForm::begin(['id'=>'borrowbook']); ?>
+<div class="frontend-views-book">
 
+    <?php $form = ActiveForm::begin(); ?>
 
-        <?= $form->field($model, 'studentId')->dropDownList($students) ?>
-        <?= $form->field($model, 'bookId')->dropDownList($books) ?>
-        <?= $form->field($model, 'borrowDate')->hiddenInput(['value'=>date('yy/m/d')])->label(false) ?>
-         <?= $form->field($model, 'expectedReturn')->widget(
-    DatePicker::className(), [
-        // inline too, not bad
-         'inline' => false,
-         // modify template for custom rendering
-       // 'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
-        'clientOptions' => [
-            'autoclose' => true,
-            'format' => 'yyyy-mm-dd'
-        ]
-]);?>
+        <?= $form->field($model, 'listingName') ?>
+        <?= $form->field($model, 'listingDesc') ?>
+        <?= $form->field($model, 'videoUrl') ?>
+        <?= $form->field($model, 'size') ?>
+        <?= $form->field($model, 'price') ?>
+        <?= $form->field($model, 'status') ?>
+        <?= $form->field($model, 'createdAt') ?>
+    
         <div class="form-group">
-            <?= Html::submitButton('Confirm', ['class' => 'btn btn-primary']) ?>
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
         </div>
-    <?php ActiveForm::end(); ?></div>
-    <!-- borrowbook -->
+    <?php ActiveForm::end(); ?>
+
+</div><!-- frontend-views-book -->
